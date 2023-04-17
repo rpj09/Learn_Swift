09f69a7de1e8aa11ada403b7e53caa6c76bab2af
rpj09 <singhripunjay09@gmail.com>
2023-04-17 10:52:53 +0530

    modified Enumerations

--- Enumerations.playground/Contents.swift
+++ Enumerations.playground/Contents.swift
@@ -9,4 +9,13 @@ enum Animals {
 
 let caty = Animals.cat
 
+caty
 
+
+if caty == Animals.cat {
+    "This is a cat"
+}else if caty == Animals.dog {
+    "This is a dog"
+}else if caty == Animals.rabbit{
+    "This is a rabbit"
+}

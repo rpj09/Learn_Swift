85914eb62dc7153d0bdb0abe603900aef035b9e2
rpj09 <singhripunjay09@gmail.com>
2023-04-17 12:59:29 +0530

    modified Enumerations

--- Enumerations.playground/Contents.swift
+++ Enumerations.playground/Contents.swift
@@ -7,7 +7,7 @@ enum Animals {
     case rabbit
 }
 
-let caty = Animals.dog
+let caty = Animals.rabbit
 
 caty
 
@@ -43,3 +43,78 @@ default: // this is kindoff works similar to "else " in "if-else"
     "this is nothin"
     break
 }
+
+
+func describeAnimal(_ animal:Animals){
+    
+    switch caty{
+    case .cat:
+        "This is a cat"
+        break
+    default: // this is kindoff works similar to "else " in "if-else"
+        "this is nothin"
+        break
+    }
+    
+}
+
+describeAnimal(Animals.dog)
+
+
+enum Shortcut {
+    case fileOrFolder(path:URL,name:String)
+    case wwwURL(path:URL)
+    case song(artist:String,songname:String)
+}
+
+ let wwwApple = Shortcut.wwwURL(path: URL(string:"www.apple.com")! )
+
+
+switch wwwApple{
+    
+case .fileOrFolder(path: let path,name: let name):
+    path
+    name
+    break
+case .wwwURL(path: let path):
+    path
+    break
+case .song(artist: let artist,songname: let songname):
+    artist
+    songname
+}
+
+// the above switch case can also be written like this
+//removed the parameters name before them , also removed the let inside the parameter bracket
+// and have put it before case value
+
+switch wwwApple{
+// we are simly unpacking/extracting values of enum instance wwwApple
+case let .fileOrFolder(path,name):
+    path
+    name
+    break
+case let .wwwURL(path):
+    path
+    break
+case let .song(artist,songname):
+    artist
+    songname
+}
+
+
+// there are other ways to extract the values , using if case statements
+
+if case let .wwwURL(path) = wwwApple{
+    path //this will return the value of case, it basically assgined the path to "path" varibale and printed it.
+    
+}
+
+
+let songna = Shortcut.song(artist: "Symphony X", songname: "Without you")
+
+if case let .song(artist,songname) = songna{
+    artist
+    songname // basically unpacking the values of enum
+}
+

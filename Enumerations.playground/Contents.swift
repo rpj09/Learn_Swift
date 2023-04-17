75cb3f64954e1c01515b417bb140daf6005258cf
rpj09 <singhripunjay09@gmail.com>
2023-04-17 11:04:23 +0530

    modified Enumerations

--- Enumerations.playground/Contents.swift
+++ Enumerations.playground/Contents.swift
@@ -7,7 +7,7 @@ enum Animals {
     case rabbit
 }
 
-let caty = Animals.cat
+let caty = Animals.dog
 
 caty
 
@@ -18,4 +18,28 @@ if caty == Animals.cat {
     "This is a dog"
 }else if caty == Animals.rabbit{
     "This is a rabbit"
+}
+
+
+switch caty{
+case .cat:
+    "This is a cat"
+    break
+case .dog:
+    "This is a dog"
+    break
+case .rabbit:
+    "This is a rabbit"
+    break
+}
+//you have to cover all the cases in a switch case
+//if you dont want to do so you can use deafult for eg.
+
+switch caty{
+case .cat:
+    "This is a cat"
+    break
+default: // this is kindoff works similar to "else " in "if-else"
+    "this is nothin"
+    break
 }

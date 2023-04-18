c6b6bd893fdb5171d76f8496ad5355b59857eb88
rpj09 <singhripunjay09@gmail.com>
2023-04-18 20:58:40 +0530

    modified Classes

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -17,6 +17,7 @@ class Persons{
         self.Name = Name
         self.age = age
     }
+    // also you cannot add prefix "mutating in classes"
     func agechange(){
         self.age+=1
     }
@@ -35,6 +36,62 @@ foo.age // here foo is a contant instance of class but still the the value chang
 // but in structures it cant be done , to do so you need to assign the instace to a variable instance i.e "var" not "let"
 
 
+
+let bar = foo
+
+foo.age
+bar.age
+
+bar.agechange()
+
+foo.age
+bar.age
+
+// if we increased the bar age , now as it refers to foo , foo's age will also be chanaged they are linked
+//because they are pointing to the same memory
+
+
+//you cannot use binary operator "==" to compare to instace of classes
+// you will have to use "==="
+if foo === bar {
+    "they point to same memory"
+}else{
+    "they point to different memories"
+}
+
+
+
+class vehicle{
+    func goVroom()->String{
+         return "vroom vroom"
+    }
+}
+
+//inheritance , cannot be done in structures
+class veh : vehicle{
+    
+}
+
+let car = veh()
+
+car.goVroom() // inheritance
+
+class  Person1{
+    private(set) var age : Int
+    init(age:Int){
+        self.age=age
+    }
+    func incAge()->Int{
+        return self.age+1
+    }
+}
+
+let man = Person1(age: 45)
+
+man.age
+
+//man.age += 1 this will result in error as age variable is set private
+// which means it can only be modified eithin the class
 
 
 

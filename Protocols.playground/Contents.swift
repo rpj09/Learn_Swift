d4827f04762cda18903420d1941745c01f080cc5
rpj09 <singhripunjay09@gmail.com>
2023-04-21 15:53:43 +0530

    Modified Protocols

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -3,3 +3,40 @@ import Foundation
 protocol pers{
     func add()
 }
+
+struct Animal:pers{
+    func add(){
+            "adding animal"
+    }
+}
+
+struct Person:pers{
+    func add(){
+            "adding person"
+    }
+}
+
+let dog = Animal()
+dog.add()
+
+let human = Person()
+human.add()
+
+
+protocol CanJump{
+    func jump()
+}
+
+extension CanJump{
+    func jump(){
+        "Jumping..."
+    }
+}
+
+struct cat : CanJump {
+    
+}
+
+let can = cat()
+
+can.jump()

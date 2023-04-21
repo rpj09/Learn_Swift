1ee122355d0f95a9a7e72b4e8fdcf76423995371
rpj09 <singhripunjay09@gmail.com>
2023-04-21 16:05:19 +0530

    Modified Protocols

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -40,3 +40,46 @@ struct cat : CanJump {
 let can = cat()
 
 can.jump()
+
+
+protocol HasName{
+    var name:String {get} //fancy way of writing let name:String
+    var age:Int {get set} // could have simply wrote var age:Int
+    //but that how we write in protocols
+    // get give you access to retrive the data
+    // set give you acess to set the data
+}
+
+extension HasName{
+    func describeme()->String{
+        "Your name is \(name) and you are \(age) years old."
+    }
+    
+    mutating func increaseage(){
+        self.age += 1
+        
+    }
+}
+
+struct Dog:HasName{
+    let name: String
+    var age: Int
+}
+
+
+var woof = Dog(name: "woof", age: 4)
+
+woof.name
+woof.age
+
+woof.age += 1
+
+woof.age
+
+
+woof.describeme()
+
+woof.increaseage()
+
+woof.age
+
